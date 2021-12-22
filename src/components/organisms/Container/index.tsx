923b72b4fe03ac127445ce35b2e3456a17558389
Le Duy Minh Pham <minh.pham@3forcom.com>
2021-12-22 14:36:26 +0700

    :construction: feat(NLW-11-o-container): implement

---
+++ src/components/organisms/Container/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import mapModifiers from 'utils/functions';
+
+interface ContainerProps {
+  fullScreen?: boolean;
+  noPaddingRight?: boolean;
+  noPaddingLeft?: boolean;
+}
+
+const Container: React.FC<ContainerProps> = ({
+  fullScreen, noPaddingRight, noPaddingLeft, children,
+}) => (
+  <div className={mapModifiers('o-container',
+    fullScreen && 'fullscreen',
+    noPaddingRight && 'noPaddingRight',
+    noPaddingLeft && 'noPaddingLeft')}
+  >
+    {children}
+
+  </div>
+);
+
+Container.defaultProps = {
+  fullScreen: false,
+  noPaddingRight: false,
+  noPaddingLeft: false,
+};
+
+export default Container;
