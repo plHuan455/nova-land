0f9d78578acfca714b13e95cc4b7c8e31b469b59
Minh Pham <minh.pham@3forcom.com>
2021-12-23 13:15:16 +0700

    :boom: feat(NLW-5-a-heading): implement

--- src/components/atoms/Heading/index.stories.tsx
+++ src/components/atoms/Heading/index.stories.tsx
@@ -6,9 +6,85 @@ import Heading from '.';
 export default {
   title: 'Components/atoms/Heading',
   component: Heading,
-  argTypes: {},
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['h1', 'h2', 'h3', 'h4', 'h5'],
+      },
+      defaultValue: 'h2',
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['default', '20x30', '32x48', '38x52', '60x72'],
+      },
+    },
+    fontWeight: {
+      control: {
+        type: 'select',
+        options: [
+          '100',
+          '200',
+          '300',
+          '400',
+          '500',
+          '600',
+          '700',
+          '800',
+          '900',
+        ],
+      },
+      defaultValue: '400',
+    },
+    color: {
+      control: {
+        type: 'select',
+        options: [
+          'white',
+          'raisinBlack',
+          'davysGrey',
+          'grayX11',
+          'whiteSmoke',
+          'snow',
+          'amazon',
+          'xanadu',
+          'goldenBrown',
+          'paleGoldenrod',
+          'juneBud',
+          'greenryb',
+          'aeroBlue',
+          'coolBlack',
+          'metallicSunburst',
+        ],
+      },
+      defaultValue: 'raisinBlack',
+    },
+    text: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'Testing Heading',
+    },
+    style: {
+      control: {
+        type: 'select',
+        options: ['uppercase', 'capitalize', 'underline', 'italic', 'center'],
+      },
+      defaultValue: 'uppercase',
+    },
+  },
 } as Meta;
 
-export const normal: Story = () => (
-  <Heading />
+export const normal: Story = ({
+  size,
+  type,
+  color,
+  fontWeight,
+  style,
+  text,
+}) => (
+  <Heading type={type} modifiers={[color, fontWeight, style, size]}>
+    {text}
+  </Heading>
 );
--- src/components/atoms/Heading/index.tsx
+++ src/components/atoms/Heading/index.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
 
+import mapModifiers from 'utils/functions';
+
+export type Sizes =
+  | '20x30'
+  | '32x48'
+  | '38x52'
+  | '60x72';
+
+export type TextStyle = (GeneralTextStyle | Sizes)[];
+
 interface HeadingProps {
+  type?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
+  content?: string;
+  modifiers?: TextStyle;
 }
 
-const Heading: React.FC<HeadingProps> = ({ children }) => (
-  <div>{children}</div>
-);
+const Heading: React.FC<HeadingProps> = ({
+  children, type = 'h2', content, modifiers,
+}) => {
+  const Element = type;
+  return (
+    <>
+      {
+        content ? (
+          <Element
+            className={mapModifiers('a-heading', modifiers)}
+            dangerouslySetInnerHTML={{ __html: content }}
+
+          />
+        ) : (
+          <Element className={mapModifiers('a-heading', modifiers)}>
+            {children}
+          </Element>
+        )
+      }
+    </>
+  );
+};
 
 Heading.defaultProps = {
+  type: 'h2',
+  modifiers: undefined,
+  content: undefined,
 };
 
 export default Heading;
