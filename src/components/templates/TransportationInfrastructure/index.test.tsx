9a7cc29261b7d14d9647a68bf3b23319e78b06f7
Nam Diep <nam.diep@3forcom.com.vn>
2021-12-24 10:54:38 +0800

    :white_check_mark: feat(feature/NLW-15-t-transportationInfrastructure): Check tsc, lint

--- src/components/templates/TransportationInfrastructure/index.test.tsx
+++ src/components/templates/TransportationInfrastructure/index.test.tsx
@@ -6,7 +6,7 @@ import TransportationInfrastructure from '.';
 describe('<TransportationInfrastructure />', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<TransportationInfrastructure />, div);
+    ReactDOM.render(<TransportationInfrastructure imgSrc="" title="" desc="" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
