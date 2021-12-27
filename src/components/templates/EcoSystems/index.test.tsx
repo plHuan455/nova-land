0492caf853c1a64982ddb2c62487501d2b4b9b55
Nam Diep <nam.diep@3forcom.com.vn>
2021-12-27 09:40:19 +0800

    :white_check_mark: feat(feature/NLW-18-ecoSystems): check tsc, lint

--- src/components/templates/EcoSystems/index.test.tsx
+++ src/components/templates/EcoSystems/index.test.tsx
@@ -6,7 +6,7 @@ import EcoSystems from '.';
 describe('<EcoSystems />', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<EcoSystems />, div);
+    ReactDOM.render(<EcoSystems title="" desc="" dataList={[]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
