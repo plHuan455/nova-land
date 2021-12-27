fc6355a07643b106e5962c78ae491dff47054600
Nam Diep <nam.diep@3forcom.com.vn>
2021-12-27 15:21:46 +0800

    :recycle: feat(feature/NLW-14-t-outStandingNumbers): Update

--- src/components/templates/OutStandingNumbers/index.tsx
+++ src/components/templates/OutStandingNumbers/index.tsx
@@ -1,37 +1,46 @@
 import React from 'react';
 
 import Heading from 'components/atoms/Heading';
-import OutStandingNumber, { OutStandingNumberProps } from 'components/molecules/OutStandingNumber';
 import Container from 'components/organisms/Container';
+import OutStandingNumber, {
+  OutStandingNumberProps,
+} from 'components/templates/OutStandingNumbers/NumberBlock';
 
 interface OutStandingNumbersProps {
   title: string;
   dataList: OutStandingNumberProps[];
 }
 
-const OutStandingNumbers: React.FC<OutStandingNumbersProps> = ({ title, dataList }) => (
+const OutStandingNumbers: React.FC<OutStandingNumbersProps> = ({
+  title,
+  dataList,
+}) => (
   <div className="t-outStandingNumbers">
     <Container>
       <Heading modifiers={['center', '700', '38x52', 'fontNoto', 'jet']}>
         {title}
       </Heading>
-      {dataList.length > 0 && (
-        <div className="t-outStandingNumbers_list">
-          {dataList.map((item, idx) => (
-            <>
-              <div key={`number-item-${idx.toString()}`} className="t-outStandingNumbers_list-item">
-                <OutStandingNumber number={item.number} desc={item.desc} />
-              </div>
-              {idx !== dataList.length - 1 && <span className="t-outStandingNumbers_list-line" />}
-            </>
-          ))}
-        </div>
-      )}
+      <div className="t-outStandingNumbers_list">
+        {dataList?.map((item, idx) => (
+          <>
+            <div
+              key={`number-item-${idx.toString()}`}
+              className="t-outStandingNumbers_list-item"
+            >
+              <OutStandingNumber number={item.number} desc={item.desc} />
+            </div>
+            {idx !== dataList.length - 1 && (
+              <span
+                className={`t-outStandingNumbers_list-line t-outStandingNumbers_list-line-${idx}`}
+              />
+            )}
+          </>
+        ))}
+      </div>
     </Container>
   </div>
 );
 
-OutStandingNumbers.defaultProps = {
-};
+OutStandingNumbers.defaultProps = {};
 
 export default OutStandingNumbers;
