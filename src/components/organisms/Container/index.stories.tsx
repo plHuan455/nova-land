9cdc00e29ddcdc8ef76d6056cfc5c18f059be660
Dan To Hoa <dan.to@3forcom.com.vn>
2021-12-27 18:11:29 +0700

    :heavy_plus_sign: add class spacing

--- src/components/organisms/Container/index.stories.tsx
+++ src/components/organisms/Container/index.stories.tsx
@@ -41,6 +41,18 @@ export const rowContainer: Story = () => (
           software like Aldus PageMaker including versions of Lorem Ipsum.
 
         </p>
+        <p className="u-mt-16 u-mt-sm-42">
+          What is Lorem Ipsum?
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book. It has survived not only five centuries, but also the leap
+          into electronic typesetting, remaining essentially unchanged.
+          It was popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages, and more recently with desktop publishing
+          software like Aldus PageMaker including versions of Lorem Ipsum.
+
+        </p>
       </CustomCol>
       <CustomCol lg={6}>
         <p>
