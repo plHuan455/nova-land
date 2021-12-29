da8997c1a9d40ffcb2c50338bdf9473af617f6f7
Tuan Huynh <huynhnhatuanqnu@gmail.com>
2021-12-29 08:36:12 +0700

    :art: feat(NLW-15-transportationInfrastructure): add font size for desc

--- src/components/templates/TransportationInfrastructure/index.tsx
+++ src/components/templates/TransportationInfrastructure/index.tsx
@@ -22,7 +22,7 @@ const TransportationInfrastructure: React.FC<
     <Container>
       <Heading modifiers={['32x48', '700', 'fontNoto', 'jet', 'uppercase', 'center']}>{title}</Heading>
       <div className="u-mt-8 u-mt-sm-16">
-        <Text type="div" modifiers={['dimGray', '400', 'center']} content={desc} />
+        <Text type="div" modifiers={['dimGray', '400', 'center', '16x24']} content={desc} />
       </div>
       <div className="u-mt-16 u-mt-sm-32">
         <Image
