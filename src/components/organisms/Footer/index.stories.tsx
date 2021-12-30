212b8b4afe09daf9e625f274284105526cd013e9
Tuan Huynh <huynhnhatuanqnu@gmail.com>
2021-12-30 14:30:00 +0700

    :tada: hotfix(not-show-storybook): add decorator for storybook agian

--- src/components/organisms/Footer/index.stories.tsx
+++ src/components/organisms/Footer/index.stories.tsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Meta, Story } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { MemoryRouter } from 'react-router-dom';
@@ -10,37 +10,34 @@ import footerMenuData from 'assets/dataDummy/footer';
 import { dummyOption } from 'container/Footer';
 import registerSchema from 'utils/schemas';
 
-export default {
-  title: 'Components/organisms/Footer',
-  component: Footer,
-  argTypes: {},
-  decorators: [(story) => <MemoryRouter>{story()}</MemoryRouter>],
-} as Meta;
-
-export const Normal: Story = () => {
-  const method = useForm<FooterRegisterFormTypes>({
-    resolver: yupResolver(registerSchema),
-    mode: 'onSubmit',
-    defaultValues: {
-      phone: '',
-      fullname: '',
-      email: '',
-    },
+storiesOf('Components/organisms/Footer', module)
+  .addDecorator((story) => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .add('normal', () => {
+    const method = useForm<FooterRegisterFormTypes>({
+      resolver: yupResolver(registerSchema),
+      mode: 'onSubmit',
+      defaultValues: {
+        phone: '',
+        fullname: '',
+        email: '',
+      },
+    });
+    const handleSubmit = (data: FooterRegisterFormTypes) => {
+      // eslint-disable-next-line no-console
+      console.log(data);
+    };
+    return (
+      <Footer
+        method={method}
+        submitForm={handleSubmit}
+        projectOptions={dummyOption}
+        externalLink={{
+          privacy: '/',
+          policy: '/',
+        }}
+        footerLink={footerMenuData}
+      />
+    );
   });
-  const handleSubmit = (data: FooterRegisterFormTypes) => {
-    // eslint-disable-next-line no-console
-    console.log(data);
-  };
-  return (
-    <Footer
-      method={method}
-      submitForm={handleSubmit}
-      projectOptions={dummyOption}
-      externalLink={{
-        privacy: '/',
-        policy: '/',
-      }}
-      footerLink={footerMenuData}
-    />
-  );
-};
