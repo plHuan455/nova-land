ae7615af4240026f585358b53782312f8fbf95f8
Tuan Huynh <huynhnhatuanqnu@gmail.com>
2022-01-05 11:07:53 +0700

    :bug: feat(NLW-40-t-featuredProject): fix crash site

--- src/components/organisms/ListFeatProjectCollapse/index.test.tsx
+++ src/components/organisms/ListFeatProjectCollapse/index.test.tsx
@@ -6,7 +6,7 @@ import ListFeatProjectCollapse from '.';
 describe('<ListFeatProjectCollapse />', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<ListFeatProjectCollapse list={[]} />, div);
+    ReactDOM.render(<ListFeatProjectCollapse indexActive={0} list={[]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
