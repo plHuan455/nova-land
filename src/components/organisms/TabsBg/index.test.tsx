3e91f15ed27518344bb5dfb5315c5076da7cfd3f
DuyLT <duy.le@3forcom.com.vn>
2022-01-21 10:01:05 +0700

    feature(NLW-89): implement p-FieldOfActivity

--- src/components/organisms/TabsBg/index.test.tsx
+++ src/components/organisms/TabsBg/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import TabsBg from '.';
+import { TabsBg } from '.';
 
 describe('<TabsBg />', () => {
   test('renders without crashing', () => {
--- src/components/templates/FieldActivityDetailsTab/index.test.tsx
+++ src/components/templates/FieldActivityDetailsTab/index.test.tsx
@@ -3,10 +3,39 @@ import ReactDOM from 'react-dom';
 
 import FieldActivityDetailsTab from '.';
 
+import bannerImg from 'assets/images/transportationInfrastructure.png';
+
+const dummyData = [
+  {
+    label: 'Tin Tập Đoàn',
+    content: {
+      imgSrc: bannerImg,
+      title: 'HẠ TẦNG GIAO THÔNG',
+      desc: 'Nâng cấp, phát triển hạ tầng giao thông tại nơi có dự án nhằm gia tăng tính kết nối vào hạ tầng giao thông trọng điểm quốc gia.',
+    },
+  },
+  {
+    label: 'Tin Thị Trường',
+    content: {
+      imgSrc: bannerImg,
+      title: 'HẠ TẦNG GIAO THÔNG',
+      desc: 'Nâng cấp, phát triển hạ tầng giao thông tại nơi có dự án nhằm gia tăng tính kết nối vào hạ tầng giao thông trọng điểm quốc gia.',
+    },
+  },
+  {
+    label: 'Tin Dự án',
+    content: {
+      imgSrc: bannerImg,
+      title: 'HẠ TẦNG GIAO THÔNG',
+      desc: 'Nâng cấp, phát triển hạ tầng giao thông tại nơi có dự án nhằm gia tăng tính kết nối vào hạ tầng giao thông trọng điểm quốc gia.',
+    },
+  },
+];
+
 describe('<FieldActivityDetailsTab />', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<FieldActivityDetailsTab />, div);
+    ReactDOM.render(<FieldActivityDetailsTab dataFieldActivity={dummyData} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
