b2f2ece0ae5c4d1da55085ed88dfa55f35ff9f72
DuyLT <duy.le@3forcom.com.vn>
2022-01-21 11:47:22 +0700

    feature(NLW-69): implement p-recuRecruitmentDetail

--- src/components/templates/BannerRecruitment/index.test.tsx
+++ src/components/templates/BannerRecruitment/index.test.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 
 import BannerRecruitment from '.';
 
+import RecruitmentBanner from 'assets/images/recruitment-banner.png';
+
 describe('<BannerRecruitment />', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<BannerRecruitment />, div);
+    ReactDOM.render(<BannerRecruitment
+      imageSrc={RecruitmentBanner}
+    />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
