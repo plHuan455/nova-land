64e60717d7b456e38bccde05c66ba26fee3f0736
DuyLT <duy.le@3forcom.com.vn>
2022-01-26 10:48:45 +0700

    hotfix(NLW-66): fix bug QC

--- src/container/NewsDetail/index.tsx
+++ src/container/NewsDetail/index.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 import BreadcrumbContainer from './breadcrumbContainer';
 import NewsDetailTemplateContainer from './newsDetailContainer';
 
-// import Section from 'components/templates/Section';
-
 const NewsDetailContainer: React.FC = () => (
   <>
     <BreadcrumbContainer />
