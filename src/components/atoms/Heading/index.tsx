8c1b48d8b8cfb8d3f5fc8195f7ca3ce03d631a80
Phuong NguyenPhamTruc <phuong.nguyen@3forcom.com.vn>
2022-01-26 08:52:12 +0700

    :sparkles: feature(feature/NLW-67-PageRecruitment)): implement Page Recruitment

--- src/components/atoms/Heading/index.tsx
+++ src/components/atoms/Heading/index.tsx
@@ -15,7 +15,9 @@ export type Sizes =
   | '60x72'
   | '64x83'
   | '24x34'
-  | '30x37-5';
+  | '30x37-5'
+  | '30x42'
+  | '64x83';
 
 type Variant = 'blueLinear';
 
--- src/components/organisms/FilterRecuitment/index.tsx
+++ src/components/organisms/FilterRecuitment/index.tsx
@@ -1,15 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import Container from '../Container';
+import { Tab, TabPanel, Tabs } from '../Tabs';
+
+import Heading from 'components/atoms/Heading';
+import Text from 'components/atoms/Text';
+import Pulldown from 'components/molecules/Pulldown';
+
+const dummyData = [
+  {
+    label: 'Tin Tập Đoàn',
+    content: 'Content 1',
+  },
+  {
+    option: [
+      {
+        label: 'abc',
+        value: 'abc',
+        id: 'abc',
+      },
+    ],
+    content: 'Content 2',
+    isPulldown: true,
+  },
+  {
+    label: 'Tin Dự án',
+    content: 'Content 3',
+  },
+];
 interface FilterRecuitmentProps {
+  heading?: string;
+  desc?: string;
 }
 
-const FilterRecuitment: React.FC<FilterRecuitmentProps> = () => (
-  <div className="t-filterRecuitment">
-    phuong
-  </div>
-);
+const FilterRecuitment: React.FC<FilterRecuitmentProps> = ({
+  heading,
+  desc,
+
+}) => {
+  const [indexActive, setIndexActive] = useState(0);
+  return (
+    <div className="t-filterRecuitment">
+      <div className="t-filterRecuitment_heading">
+        <Heading modifiers={['400', '64x83', 'center', 'fontNoto', 'white']} content={heading} />
+      </div>
+      <div className="t-filterRecuitment_desc">
+        <Text modifiers={['300', '18x28', 'center', 'fontLexend', 'white']} content={desc} />
+      </div>
+      <div className="t-filterRecuitment_tab">
+        <Container>
+          <Tabs variableMutate={indexActive}>
+            {
+              dummyData.map((item, index) => (
+                <>
+                  {item.isPulldown
+                    ? <Pulldown options={item.option} handleChange={() => setIndexActive(index)} />
+                    : (
+                      <Tab
+                        key={`tab-${index.toString()}`}
+                        label={item.label}
+                        active={index === indexActive}
+                        handleClick={() => setIndexActive(index)}
+                      />
+                    )}
+                </>
+              ))
+            }
+          </Tabs>
+          {
+            dummyData.map((item, index) => (
+              <TabPanel key={`tab-panel-${index.toString()}`} active={index === indexActive}>
+                {item.content}
+              </TabPanel>
+            ))
+          }
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 FilterRecuitment.defaultProps = {
+  heading: undefined,
+  desc: undefined,
 };
 
 export default FilterRecuitment;
