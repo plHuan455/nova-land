680be137c54855e19e9103ba342dd01901ee7be8
Chau Truong Phu <chau.truong@3forcom.com.vn>
2022-01-27 14:40:57 +0700

    ✨ feat(NLW-128): Implement T-TransactionList

---
+++ src/components/templates/TransactionList/index.stories.tsx
@@ -0,0 +1,28 @@
+import { Story, Meta } from '@storybook/react';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import TransactionList from '.';
+
+import dataTransactionList from 'assets/dataDummy/transactionList';
+
+export default {
+  title: 'Components/templates/TransactionList',
+  component: TransactionList,
+  argTypes: {},
+} as Meta;
+
+export const normal: Story = () => (
+  <div style={{ height: '100vh' }}>
+    <Router>
+      <TransactionList
+        title="Các sàn giao dịch của Novaland group"
+        dataTransactionList={dataTransactionList}
+        totalPage={1}
+        total={9}
+        currentPage={1}
+        handleChangePage={(page: number) => (page)}
+      />
+    </Router>
+  </div>
+);
