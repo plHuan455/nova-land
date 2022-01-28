ab10a133130d79cd11ca7bc652e8931e95986e09
Le Duy Minh Pham <minh.pham@3forcom.com>
2022-01-28 11:34:34 +0700

    :tada: feat(NLW-156-system-api): implement header logo

--- src/container/Header/index.tsx
+++ src/container/Header/index.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 import logo from 'assets/images/logo.png';
 import Header from 'components/organisms/Header';
 import { useAppSelector } from 'store/hooks';
+import { BASE_URL } from 'utils/functions';
 
 interface HeaderContainerProps {
 }
 
 const HeaderContainer: React.FC<HeaderContainerProps> = () => {
   const menuList = useAppSelector((state) => state.menus.header);
+  const system = useAppSelector((state) => state.system.dataSystem);
 
   return (
     <Header
       headerMenu={menuList}
-      logoSrc={logo}
+      logoSrc={`${BASE_URL}${system?.logo}` || logo}
     />
   );
 };
