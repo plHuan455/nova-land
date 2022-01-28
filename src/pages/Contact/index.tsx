5e47b6b7d7d5a2c048d45e150d923831790a61f9
Tuan Nguyen <tuan.nguyen@3forcom.com.vn>
2022-01-28 09:46:29 +0700

    :hammer: enhance code

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import ContactContainer from 'container/Contact';
+import MainLayoutContainer from 'container/MainLayout';
 
 const Contact: React.FC = () => (
-  <div className="p-contact">
-    <ContactContainer />
-  </div>
+  <MainLayoutContainer>
+    <div className="p-contact">
+      <ContactContainer />
+    </div>
+  </MainLayoutContainer>
 );
 
 export default Contact;
