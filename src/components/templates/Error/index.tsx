d3d90cbb57b7931376a069499cfc8d27b3d33fbd
Nam Diep <nam.diep@3forcom.com.vn>
2022-01-28 14:12:17 +0800

    :art: feat(feature/NLW-123-p-error): Update error template

--- src/components/templates/Error/index.tsx
+++ src/components/templates/Error/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/require-default-props */
 
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import error404Img from 'assets/images/errors/error-404.png';
 import Button from 'components/atoms/Button';
@@ -12,44 +12,83 @@ import Container from 'components/organisms/Container';
 import Section from 'components/templates/Section';
 
 export interface ErrorProps {
-  title: string;
-  description: string;
+  statusCode: number;
+  titlePage?: string;
+  description?: string;
   btnHomeText: string;
-  image?: string;
   linkButton?: string;
   targetButton?: string;
 }
 
 const Error: React.FC<ErrorProps> = ({
-  title,
+  statusCode,
+  titlePage,
   description,
   btnHomeText,
-  image,
   linkButton,
   targetButton,
-}) => (
-  <div className="t-error">
-    <Section>
-      <Container>
-        <div className="t-error_imgError">
-          <Image ratio="641x211" src={image || error404Img} alt="imageError" />
-        </div>
-        <div className="t-error_wrapContent">
-          <Heading modifiers={['jet', '30x37-5', '600', 'center']}>
-            {title}
-          </Heading>
-          <Text modifiers={['jet', '20x28', '400', 'center']}>
-            {description}
-          </Text>
-          <div className="t-error_btn">
-            <Link href={linkButton} target={targetButton}>
-              <Button>{btnHomeText}</Button>
-            </Link>
+}) => {
+  const { title, desc, imgSrc } = useMemo(() => {
+    switch (statusCode) {
+      case 403:
+        return {
+          imgSrc: error404Img,
+          title: titlePage || 'Rất tiếc, Quý Khách không thể truy cập trang này.',
+          desc:
+            description || 'Vui lòng trở về trang chủ hoặc liên hệ với chúng tôi để được hỗ trợ.',
+        };
+      case 500:
+        return {
+          imgSrc: error404Img,
+          title: titlePage || 'Lỗi máy chủ nội bộ ',
+          desc:
+            description || 'Rất tiếc, một vài lỗi không mong muốn đã xảy ra, </br> chúng tôi đang cố gắng khắc phục sự cố. Quý Khách xin vui lòng thử lại sau.',
+        };
+      case 503:
+        return {
+          imgSrc: error404Img,
+          title: titlePage || 'Rất tiếc, hệ thống đang được bảo trì',
+          desc:
+            description || 'Trang thông tin Quý Khách tìm kiếm đang trong quá trình bảo trì, </br> vui lòng thử lại sau.',
+        };
+
+      default:
+        return {
+          imgSrc: error404Img,
+          title: titlePage || 'Rất tiếc, chúng tôi không tìm thấy trang này',
+          desc:
+            description || 'Vui lòng trở về trang chủ hoặc liên hệ với chúng tôi để được hỗ trợ.',
+        };
+    }
+  }, [description, statusCode, titlePage]);
+  return (
+    <div className="t-error">
+      <Section>
+        <Container>
+          <div className="t-error_imgError">
+            <Image ratio="641x211" src={imgSrc} alt="imageError" />
           </div>
-        </div>
-      </Container>
-    </Section>
-  </div>
-);
+          <div className="t-error_wrapContent">
+            <Heading modifiers={['jet', '30x37-5', '600', 'center']}>
+              {title}
+            </Heading>
+            <div className="t-error_desc">
+              <Text modifiers={['jet', '20x28', '400', 'center']}>
+                {desc}
+              </Text>
+            </div>
+            <div className="t-error_btn">
+              <Link href={linkButton} target={targetButton}>
+                <Button>
+                  <Text modifiers={['700', '18x28']}>{btnHomeText}</Text>
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Container>
+      </Section>
+    </div>
+  );
+};
 
 export default Error;
