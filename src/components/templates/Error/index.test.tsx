0c9c9d9fea3d881f261d2449bcf52bacd79eb49e
Phuc Phan Hong <phuc.phan@3forcom.com>
2022-01-30 13:17:54 +0700

    :bug: fix(bds-slug): hard code slug

--- src/components/templates/Error/index.test.tsx
+++ src/components/templates/Error/index.test.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import Error from '.';
 
 describe('<Error />', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Router><Error btnHomeText="" description="" title="" /></Router>, div);
+    ReactDOM.render(
+      <Router>
+        <Error statusCode={404} btnHomeText="Trở về trang chủ" />
+      </Router>,
+      div,
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
