92ba046d23f2900ddfc25b5e25d933c7a4ada9b2
Tuan Huynh <tuan.huynh@3forcom.com>
2022-02-11 11:46:44 +0700

    :art: feat(NLW-101-Page-Corporate-Governance): refactor file

--- src/container/CorporateGovernance/index.tsx
+++ src/container/CorporateGovernance/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import BannerRecruitmentContainer from './bannerRecruitmentContainer';
-import BreadcrumbContainer from './breadcrumbContainer';
 import CorporateGovernanceTableContainer from './corporateGovernanceTable';
 
+import Breadcrumb from 'components/molecules/Breadcrumb';
 import Section from 'components/templates/Section';
 import HelmetContainer from 'container/helmet';
 
@@ -11,7 +11,24 @@ const CorporateGovernanceContainer: React.FC = () => (
   <>
     <HelmetContainer />
     <BannerRecruitmentContainer />
-    <BreadcrumbContainer />
+    <div className="p-corporateGovernance_breadcrumb u-mt-md-24 u-mt-14 u-mb-md-60 u-mb-25">
+      <Breadcrumb
+        breadcrumbs={[
+          {
+            pathName: '/',
+            title: 'Trang chủ',
+          },
+          {
+            pathName: '/quan-he-dau-tu',
+            title: 'Quan hệ đầu tư',
+          },
+          {
+            pathName: '/quan-tri-danh-nghiep',
+            title: 'Quản Trị Danh Nghiệp',
+          },
+        ]}
+      />
+    </div>
     <Section modifiers="noPt">
       <CorporateGovernanceTableContainer />
     </Section>
