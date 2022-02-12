b6968907cc1a257f352ae817ea83597ab7f77cca
Chau Truong Phu <chau.truong@3forcom.com.vn>
2022-02-12 09:38:55 +0700

    🔨 hotfix(NLW-244): Update InfoNews Card

--- src/components/templates/EventList/index.test.tsx
+++ src/components/templates/EventList/index.test.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import EventList from '.';
 
 describe('<EventList />', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<EventList
-      eventList={[]}
-      totalPage={10}
-      // eslint-disable-next-line no-console
-      handleGetPage={(p) => console.log(p)}
-    />, div);
+    ReactDOM.render(
+      <BrowserRouter>
+        <EventList
+          eventList={[]}
+          totalPage={0}
+          handleGetPage={() => {}}
+        />
+      </BrowserRouter>,
+      div,
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
