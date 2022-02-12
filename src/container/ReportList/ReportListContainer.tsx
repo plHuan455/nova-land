dc305bb77cbba87e6b453588c915a2f5b21c0534
Chau Truong Phu <chau.truong@3forcom.com.vn>
2022-02-12 09:09:54 +0700

    🔨 hotfix(NLW-243): Update ReportList

--- src/container/ReportList/ReportListContainer.tsx
+++ src/container/ReportList/ReportListContainer.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import reportList from 'assets/dataDummy/reportList';
 import ReportList from 'components/templates/ReportList';
+import Section from 'components/templates/Section';
 
 const ReportListTemplateContainer: React.FC = () => (
   <div className="p-reportList_list">
-    <ReportList
-      totalPage={5}
-      currentPage={1}
-      reportList={reportList.ReportListDumy}
-    />
+    <Section modifiers="noPt">
+      <ReportList
+        totalPage={5}
+        currentPage={1}
+        reportList={reportList.ReportListDumy}
+      />
+    </Section>
   </div>
 );
 
