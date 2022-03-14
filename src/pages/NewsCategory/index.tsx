c2b1f44c8af4db0cc43b1786f53c4888ab572f4b
Chau Phu Truong <chau.truong@3forcom.com.vn>
2022-03-14 13:45:06 +0700

    🔨 bugfix: Fix Duplicate Footer

--- src/pages/NewsCategory/index.tsx
+++ src/pages/NewsCategory/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import MainLayout from 'components/templates/MainLayout';
 import NewsCategoryContainer from 'container/NewsCategory';
 
 const NewsCategory: React.FC = () => (
-  <MainLayout>
-    <div className="p-newsCategory">
-      <NewsCategoryContainer />
-    </div>
-  </MainLayout>
+  <div className="p-newsCategory">
+    <NewsCategoryContainer />
+  </div>
 );
 
 export default NewsCategory;
