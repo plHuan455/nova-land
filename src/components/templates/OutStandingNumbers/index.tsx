68b094b43c2f05a146a5f89b07cd384c72287745
Hoang Cong Phan <hoang.phancong@3forcom.com.vn>
2022-03-21 09:50:33 +0700

    🐛 (NLW-309): Convert font noto to calibri

--- src/components/templates/OutStandingNumbers/index.tsx
+++ src/components/templates/OutStandingNumbers/index.tsx
@@ -17,11 +17,11 @@ export interface NumberBlockProps {
 
 const NumberBlock: React.FC<NumberBlockProps> = ({ number, desc }) => (
   <div>
-    <Heading modifiers={['52x65', '600', 'blueLinear', 'center', 'fontNoto']}>
+    <Heading modifiers={['52x65', '600', 'blueLinear', 'center', 'fontCalibri']}>
       {number}
     </Heading>
     <div className="u-mt-sm-8 u-mt-md-16">
-      <Text modifiers={['16x20', '300', 'dimGray', 'center', 'uppercase']}>{desc}</Text>
+      <Text modifiers={['16x20', '300', 'dimGray', 'center', 'uppercase', 'fontCalibri']}>{desc}</Text>
     </div>
   </div>
 );
