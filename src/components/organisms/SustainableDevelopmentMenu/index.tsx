8d5a4f6845c2277e2f6ab2baf6e0441f13928bc7
Liem Pham <liem.pham@3forcom.com.vn>
2022-03-22 15:41:33 +0700

    :sparkles: feat(NLW-304-UI-Sustainable-development-page): Implement UI menu

--- src/components/organisms/SustainableDevelopmentMenu/index.tsx
+++ src/components/organisms/SustainableDevelopmentMenu/index.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 
 import Container from '../Container';
 
+import Icon from 'components/atoms/Icon';
 import Link from 'components/atoms/Link';
 import { MenuItemDataTypes } from 'services/menus/types';
 import mapModifiers, { checkExternalUrl } from 'utils/functions';
@@ -19,68 +20,80 @@ const SustainableDevelopmentMenu: React.FC<SustainableDevelopmentMenuProps> = ({
   // HOOK
   const menuRef = useRef<HTMLUListElement>(null);
   return (
-    <div className="o-sustainableDevelopmentMenu">
+    <div className="o-sdMenu">
       <Container>
-        <div className="o-sustainableDevelopmentMenu_menu">
+        <div className="o-sdMenu_menu">
           <ul
             ref={menuRef}
-            className={mapModifiers('o-sustainableDevelopmentMenu_nav', isOpenMenu && 'active')}
+            className={mapModifiers(
+              'o-sdMenu_nav',
+              isOpenMenu && 'active',
+            )}
           >
             {headerDataMenus
-          && headerDataMenus.map((val, idx) => {
-            const link = `${
-              val.reference && val.reference.slug !== '/'
-                ? `/${val.reference.slug}`
-                : val.reference?.slug || val.link
-            }`;
-            return (
-              <li
-                className={`o-sustainableDevelopmentMenu_nav_item${
-                  val?.subMenu?.length ? ' o-sustainableDevelopmentMenu_hasChild' : ''
-                }`}
-                key={idx.toString()}
-              >
-                <Link
-                  href={link}
-                  customClassName="o-sustainableDevelopmentMenu_nav_link"
-                  handleClick={() => setIsOpenMenu(!isOpenMenu)}
-                  target={val.target}
-                  useExternal={
-                    val.reference?.slug ? false : checkExternalUrl(val.link)
-                  }
-                >
-                  {val.title}
-                </Link>
-                {val?.subMenu?.length && (
-                <div className="o-sustainableDevelopmentMenu_dropdown">
-                  <div className="o-sustainableDevelopmentMenu_dropdown_wrapper">
-                    <ul className="o-sustainableDevelopmentMenu_dropdown_content">
-                      {val.subMenu.map((itemSubmenu, index) => (
-                        <li
-                          className={mapModifiers(
-                            'o-sustainableDevelopmentMenu_dropdown_item',
-                          )}
-                          key={`${itemSubmenu.title}${index.toString()}`}
-                        >
-                          <Link
-                            href={`${itemSubmenu.reference
-                                        && itemSubmenu.reference.slug}` || '/'}
-                            handleClick={() => {
-                              setIsOpenMenu(false);
-                            }}
-                            customClassName="o-sustainableDevelopmentMenu_dropdown_nav_link"
-                          >
-                            {itemSubmenu.title}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-                )}
-              </li>
-            );
-          })}
+              && headerDataMenus.map((val, idx) => {
+                const link = `${
+                  val.reference && val.reference.slug !== '/'
+                    ? `/${val.reference.slug}`
+                    : val.reference?.slug || val.link
+                }`;
+                return (
+                  <li
+                    className={`o-sdMenu_nav_item${
+                      val?.subMenu?.length
+                        ? ' o-sdMenu_hasChild'
+                        : ''
+                    }`}
+                    key={idx.toString()}
+                  >
+                    <Link
+                      href={link}
+                      customClassName="o-sdMenu_nav_link"
+                      handleClick={() => setIsOpenMenu(!isOpenMenu)}
+                      target={val.target}
+                      useExternal={
+                        val.reference?.slug ? false : checkExternalUrl(val.link)
+                      }
+                    >
+                      {val.title}
+                    </Link>
+                    {val?.subMenu?.length && (
+                      <div className="o-sdMenu_dropdown">
+                        <div className="o-sdMenu_dropdown_wrapper">
+                          <ul className="o-sdMenu_dropdown_content">
+                            {val.subMenu.map((itemSubmenu, index) => (
+                              <li
+                                className={mapModifiers(
+                                  'o-sdMenu_dropdown_item',
+                                )}
+                                key={`${itemSubmenu.title}${index.toString()}`}
+                              >
+                                <div className="o-sdMenu_dropdown_item_icon u-mr-8">
+                                  <Icon iconName="novaMark" size="15x19" />
+                                </div>
+                                <Link
+                                  href={
+                                    `${
+                                      itemSubmenu.reference
+                                      && itemSubmenu.reference.slug
+                                    }` || '/'
+                                  }
+                                  handleClick={() => {
+                                    setIsOpenMenu(false);
+                                  }}
+                                  customClassName="o-sdMenu_dropdown_nav_link"
+                                >
+                                  {itemSubmenu.title}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      </div>
+                    )}
+                  </li>
+                );
+              })}
           </ul>
         </div>
       </Container>
